411882f707523218f69fe3053d3fff2399e45062
Kyle Felter <kfelter@nvidia.com>
2026-09-01 23:17:51 -0500

    fix(rest-api): Document Machine delete rate limits
    Signed-off-by: Kyle Felter <kfelter@nvidia.com>

--- rest-api/sdk/standard/api_machine.go
+++ rest-api/sdk/standard/api_machine.go
@@ -367,6 +367,17 @@ func (a *MachineAPIService) DeleteMachineExecute(r ApiDeleteMachineRequest) (*Me
 			newErr.model = v
 			return localVarReturnValue, localVarHTTPResponse, newErr
 		}
+		if localVarHTTPResponse.StatusCode == 429 {
+			var v NICoAPIError
+			err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
+				return localVarReturnValue, localVarHTTPResponse, newErr
+			}
+			newErr.error = formatErrorMessage(localVarHTTPResponse.Status, &v)
+			newErr.model = v
+			return localVarReturnValue, localVarHTTPResponse, newErr
+		}
 		if localVarHTTPResponse.StatusCode == 422 {
 			var v NICoAPIError
 			err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
